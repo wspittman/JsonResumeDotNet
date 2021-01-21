349433480aafee7e903d443ae61802bfc9318ea7
William Pittman <wspittman@gmail.com>
2021-01-21 13:13:59 -0800

    Resume.Tests: CertificateTests

--- Resume.Tests/Models/CertificateTests.cs
+++ Resume.Tests/Models/CertificateTests.cs
@@ -37,5 +37,42 @@ namespace Resume.Tests
             var certificateConstructed = new Certificate() { Name = name };
             Utils.ValidatePropertyPair(certificateFromJson, certificateConstructed, name, x => (x as Certificate).Name);
         }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("1/1/0001")]
+        [TestCase("12/31/9999")]
+        [TestCase("1989-06-12")]
+        public void DateTest(string dateString)
+        {
+            DateTime? parsedDate = DateTime.TryParse(dateString, out DateTime parsed) ? parsed : (DateTime?)null;
+
+            var certificateFromJson = FromJson(date: dateString);
+            var certificateConstructed = new Certificate() { Date = parsedDate };
+            Utils.ValidatePropertyPair(certificateFromJson, certificateConstructed, parsedDate, x => (x as Certificate).Date);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("http://example.com")]
+        public void UrlTest(string urlString)
+        {
+            // Match deserialization corner cases by using UriTypeConverter instead of Uri.TryCreate 
+            var parsedUri = urlString == null ? null : new UriTypeConverter().ConvertFromString(urlString) as Uri;
+
+            var certificateFromJson = FromJson(url: urlString);
+            var certificateConstructed = new Certificate() { Url = parsedUri };
+            Utils.ValidatePropertyPair(certificateFromJson, certificateConstructed, parsedUri, x => (x as Certificate).Url);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("CNCF")]
+        public void IssuerTest(string issuer)
+        {
+            var certificateFromJson = FromJson(issuer: issuer);
+            var certificateConstructed = new Certificate() { Issuer = issuer };
+            Utils.ValidatePropertyPair(certificateFromJson, certificateConstructed, issuer, x => (x as Certificate).Issuer);
+        }
     }
 }
