79f4c6f22f309ce0e22200eb5564daaa58617de6
William Pittman <wspittman@gmail.com>
2021-01-21 16:34:56 -0800

    Resume: Set null and default value serializer settings to Ignore

--- Resume/Resume.cs
+++ Resume/Resume.cs
@@ -5,14 +5,22 @@ namespace Resume
 {
     public partial class Resume
     {
+        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings()
+        {
+            // Error when JSON has a property that class doesn't
+            //MissingMemberHandling = MissingMemberHandling.Error
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore,
+        };
+
         public static Resume FromJson(string json)
         {
-            return JsonConvert.DeserializeObject<Resume>(json);
+            return JsonConvert.DeserializeObject<Resume>(json, settings);
         }
 
         public string ToJson()
         {
-            return JsonConvert.SerializeObject(this);
+            return JsonConvert.SerializeObject(this, settings);
         }
     }
 }
