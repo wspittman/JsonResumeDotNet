9c76e89a2f81b83416c6c2f0bacbad6b1e2af53a
William Pittman <wspittman@gmail.com>
2021-01-23 13:47:14 -0800

    Resume: Update Resume.FromJson with parameters to control error throwing and if unknown members are considered errors.

--- Resume.Tests/Utils.cs
+++ Resume.Tests/Utils.cs
@@ -10,13 +10,16 @@ namespace Resume.Tests
         public static Resume FromJson(string format, params object[] args)
         {
             var prepped = args.Select(str => (str as string == "{}") ? "{}" : JsonConvert.SerializeObject(str)).ToArray();
-            return Resume.FromJson(string.Format(format, prepped));
+            return Resume.FromJson(string.Format(format, prepped), throwOnError: false, errorOnUnknownMember: true);
         }
 
-        public static void ValidatePropertyPair(Resume parsed, Resume constructed, object expectedPropertyValue, Func<Resume, object> getProperty)
+        public static void ValidatePropertyPair(Resume parsed, Resume constructed, object expectedPropertyValue, Func<Resume, object> getProperty, int expectedParsingErrorCount = 0)
         {
             // Is the property value what we expect?
             Assert.AreEqual(expectedPropertyValue, getProperty(parsed));
+
+            // Does the parsed object have the expected number of parsing errors?
+            Assert.AreEqual(expectedParsingErrorCount, parsed.ParsingErrors.Count);
 
             // Do the parsed and constructed objects both have matching values?
             Assert.AreEqual(getProperty(parsed), getProperty(constructed));
--- Resume/Resume.cs
+++ Resume/Resume.cs
@@ -1,26 +1,59 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 
 namespace Resume
 {
     public partial class Resume
     {
-        private static readonly JsonSerializerSettings settings = new JsonSerializerSettings()
-        {
-            // Error when JSON has a property that class doesn't
-            //MissingMemberHandling = MissingMemberHandling.Error
-            NullValueHandling = NullValueHandling.Ignore,
-            DefaultValueHandling = DefaultValueHandling.Ignore,
-        };
+        [JsonIgnore]
+        public List<Exception> ParsingErrors { get; private set; } = new List<Exception>();
 
-        public static Resume FromJson(string json)
+        public static Resume FromJson(string json, bool throwOnError = true, bool errorOnUnknownMember = false)
         {
-            return JsonConvert.DeserializeObject<Resume>(json, settings);
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return new Resume();
+            }
+
+            var settings = GetSettings(errorOnUnknownMember);
+            var errors = new List<Exception>();
+
+            settings.Error = (_, args) =>
+            {
+                // If this is the source of the error, rather than a parent collection/object, log it
+                if (args.CurrentObject == args.ErrorContext.OriginalObject)
+                {
+                    errors.Add(args.ErrorContext.Error);
+                }
+
+                // If we don't want to throw, set Handled=true to prevent JsonConvert from throwing
+                if (!throwOnError)
+                {
+                    args.ErrorContext.Handled = true;
+                }
+            };
+
+            var resume = JsonConvert.DeserializeObject<Resume>(json, settings);
+            
+            resume.ParsingErrors = errors;
+            
+            return resume;
         }
 
         public string ToJson()
         {
-            return JsonConvert.SerializeObject(this, settings);
+            return JsonConvert.SerializeObject(this, GetSettings());
+        }
+
+        private static JsonSerializerSettings GetSettings(bool errorOnUnknownMember = false)
+        {
+            return new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+                MissingMemberHandling = errorOnUnknownMember ? MissingMemberHandling.Error : MissingMemberHandling.Ignore,
+            };
         }
     }
 }
