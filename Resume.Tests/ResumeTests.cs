3c42f8a5dd6859233cc92e8c84579bfaaa346444
William Pittman <wspittman@gmail.com>
2021-02-18 18:50:06 -0800

    Resume.Tests: Add tests for invalid Json and parsing errors

--- Resume.Tests/ResumeTests.cs
+++ Resume.Tests/ResumeTests.cs
@@ -3,26 +3,21 @@ using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace Resume.Tests
 {
     public class ResumeTests
     {
         private const string DirectoryName = "SampleJson";
-        private static string shallowNulls;
-        private static string deepNulls;
-        private static string unknownMembers;
-        private static string sample;
+
+        private static Dictionary<string, string> files;
 
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
-            shallowNulls = File.ReadAllText(Path.Combine(DirectoryName, "shallowNulls.json"));
-            deepNulls = File.ReadAllText(Path.Combine(DirectoryName, "deepNulls.json"));
-            unknownMembers = File.ReadAllText(Path.Combine(DirectoryName, "unknownMembers.json"));
-            sample = File.ReadAllText(Path.Combine(DirectoryName, "sample.json"));
+            files = Directory.GetFiles(DirectoryName, "*.json").ToDictionary(file => Path.GetFileNameWithoutExtension(file), file => File.ReadAllText(file));
         }
 
         [Test]
@@ -41,7 +36,7 @@ namespace Resume.Tests
         [Test]
         public void ShallowNullsTest([Values] bool throwOnError, [Values] bool errorOnUnknownMember)
         {
-            var fromJson = Resume.FromJson(shallowNulls, throwOnError, errorOnUnknownMember);
+            var fromJson = Resume.FromJson(files["shallowNulls"], throwOnError, errorOnUnknownMember);
             var constructed = new Resume();
 
             Utils.ValidateResume(fromJson, constructed);
@@ -50,7 +45,7 @@ namespace Resume.Tests
         [Test]
         public void DeepNullsTest([Values] bool throwOnError, [Values] bool errorOnUnknownMember)
         {
-            var fromJson = Resume.FromJson(deepNulls, throwOnError, errorOnUnknownMember);
+            var fromJson = Resume.FromJson(files["deepNulls"], throwOnError, errorOnUnknownMember);
             var stringList = new List<string>() { null, null };
 
             var constructed = new Resume()
@@ -84,12 +79,12 @@ namespace Resume.Tests
 
             try
             {
-                fromJson = Resume.FromJson(unknownMembers, throwOnError, errorOnUnknownMember);
+                fromJson = Resume.FromJson(files["unknownMembers"], throwOnError, errorOnUnknownMember);
             }
             catch (JsonSerializationException)
             {
-                Assert.IsTrue(errorOnUnknownMember, "ErrorOnUnknownMember = false, but JsonSerializationException exception thrown");
-                Assert.IsTrue(throwOnError, "ThrowOnError = false, but JsonSerializationException exception thrown");
+                Assert.IsTrue(errorOnUnknownMember, "ErrorOnUnknownMember = false, but JsonSerializationException thrown");
+                Assert.IsTrue(throwOnError, "ThrowOnError = false, but JsonSerializationException thrown");
                 return;
             }
 
@@ -121,12 +116,52 @@ namespace Resume.Tests
         [Test]
         public void SampleTest([Values] bool throwOnError, [Values] bool errorOnUnknownMember)
         {
-            var fromJson = Resume.FromJson(sample, throwOnError, errorOnUnknownMember);
+            var fromJson = Resume.FromJson(files["sample"], throwOnError, errorOnUnknownMember);
 
             // Yeah, I'm not recreating the sample by hand here, we'll just reparse and compare for now
-            var reparse = JObject.Parse(sample).ToObject<Resume>();
+            var reparse = JObject.Parse(files["sample"]).ToObject<Resume>();
 
             Utils.ValidateResume(fromJson, reparse);
+        }
+
+        [Test]
+        public void InvalidJsonTest([Values] bool throwOnError, [Values] bool errorOnUnknownMember)
+        {
+            Resume fromJson;
+
+            try
+            {
+                fromJson = Resume.FromJson("{{{", throwOnError, errorOnUnknownMember);
+            }
+            catch (JsonReaderException)
+            {
+                Assert.IsTrue(throwOnError, "ThrowOnError = false, but JsonReaderException thrown");
+                return;
+            }
+
+            var constructed = new Resume();
+
+            Utils.ValidateResume(fromJson, constructed, 2);
+        }
+
+        [Test]
+        public void ParsingErrorsTest([Values] bool throwOnError, [Values] bool errorOnUnknownMember)
+        {
+            Resume fromJson;
+
+            try
+            {
+                fromJson = Resume.FromJson(files["errors"], throwOnError, errorOnUnknownMember);
+            }
+            catch (JsonSerializationException)
+            {
+                Assert.IsTrue(throwOnError, "ThrowOnError = false, but JsonSerializationException thrown");
+                return;
+            }
+
+            var fromJsonCleaned = Resume.FromJson(files["errorsCleaned"], throwOnError, errorOnUnknownMember);
+
+            Utils.ValidateResume(fromJson, fromJsonCleaned, 24);
         }
     }
 }
